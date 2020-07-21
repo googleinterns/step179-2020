ef18d0df603a369a712728b9a1736dd59c653045
meghakak <meghakak1@gmail.com>
2020-07-21 15:50:58 +0000

    Move head's div tag to body.

--- capstone/src/main/java/com/google/sps/servlets/ClubServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/ClubServlet.java
@@ -1,20 +1,21 @@
-package com.google.sps.servlets;
+// package com.google.sps.servlets;
 
-import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+// import java.io.IOException;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example club content*/
-@WebServlet("/clubs")
-public class DataServlet extends HttpServlet {
-  private Map<String, Club> clubs = new HashMap<String, Club>();
-  
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    setUp();
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
-}
+// /** Servlet that returns some example club content*/
+// @WebServlet("/clubs")
+// public class DataServlet extends HttpServlet {
+//   private Map<String, Club> clubs = new HashMap<String, Club>();
+
+//   @Override
+//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+// {
+//     setUp();
+//     response.setContentType("text/html;");
+//     response.getWriter().println("<h1>Hello world!</h1>");
+//   }
+// }
