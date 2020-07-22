2f2c8c89ba8cbf6d14f0435626cd88699630e4df
Kevin Shao <knshao@gmail.com>
2020-07-22 05:13:37 +0000

    Implement Java servlets to serve hard-coded announcements

---
+++ capstone/src/main/java/com/google/sps/servlets/AnnouncementsServlet.java
@@ -0,0 +1,28 @@
+package com.google.sps.servlets;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns some example club content */
+@WebServlet("/announcements")
+public class AnnouncementsServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Only access Club 1, hardcoded
+    Gson gson = new Gson();
+    ImmutableList<String> announcements =
+        PrototypeClubs.PROTOTYPE_CLUBS_MAP.get(PrototypeClubs.CLUB_1).getAnnouncements();
+    String json = gson.toJson(announcements);
+    response.setContentType("text/html;");
+    response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {}
+}
