200156f4bca40d0904eb238b24adb2c8b8ef9946
Kevin Shao <knshao@gmail.com>
2020-07-22 06:00:49 +0000

    Add back end support for serving data for explore page

--- capstone/src/main/java/com/google/sps/servlets/ExploreServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/ExploreServlet.java
@@ -8,21 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example club content */
-@WebServlet("/announcements")
-public class AnnouncementsServlet extends HttpServlet {
-
+/** Servlet that returns some example club content. */
+@WebServlet("/explore")
+public class ExploreServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Only access Club 1, hardcoded
     Gson gson = new Gson();
-    ImmutableList<String> announcements =
-        PrototypeClubs.PROTOTYPE_CLUBS_MAP.get(PrototypeClubs.CLUB_1).getAnnouncements();
-    String json = gson.toJson(announcements);
+    // Only accesses Clubs 1, 2, 3 for now
+    ImmutableList<Club> clubs =
+        ImmutableList.of(
+            PrototypeClubs.PROTOTYPE_CLUBS_MAP.get(PrototypeClubs.CLUB_1),
+            PrototypeClubs.PROTOTYPE_CLUBS_MAP.get(PrototypeClubs.CLUB_2),
+            PrototypeClubs.PROTOTYPE_CLUBS_MAP.get(PrototypeClubs.CLUB_3));
+    String json = gson.toJson(clubs);
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {}
 }
