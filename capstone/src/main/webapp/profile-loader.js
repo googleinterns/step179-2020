d2178725a84c6bb9a078a470dd7a7af52d68b219
meghakak <meghakak1@gmail.com>
2020-08-05 17:31:31 +0000

    Update UI of profile page based on feedback.

--- capstone/src/main/webapp/profile-loader.js
+++ capstone/src/main/webapp/profile-loader.js
@@ -3,12 +3,13 @@ showOrHideProfile();
 /** Fetch login status and show or hide profile accordingly */
 function showOrHideProfile() {
   fetch('/auth').then(response => response.text()).then((loginStatus) => {
-    var authContent = document.getElementById('profile-content');
+    var authContent = document.getElementById('top-navigation');
     if (loginStatus.includes('Logout')) {
-      authContent.innerHTML += loginStatus;
+      authContent.innerHTML = loginStatus + authContent.innerHTML;
       getStudentInfo();
     }
     else {
+      authContent = document.getElementById('profile-content');
       authContent.innerHTML = loginStatus;
     }
   })
@@ -19,9 +20,12 @@ function getStudentInfo() {
   fetch('/student-data').then(response => response.json()).then((info) => {  
     var studentInfo = info['student'];
     var announcements = info['announcements'];
+    const editableName = 'class="edit-name" contenteditable="true">';
+    const pencilIcon = '  <i class="far fa-edit"></i>';
+
     // Update profile name
     var profileTitle = document.getElementById('heading');
-    profileTitle.innerHTML += studentInfo['name'];
+    profileTitle.innerHTML += '<span id="edit-name"' + editableName + studentInfo['name'] + pencilIcon + '</span>';
 
     // Update profile club list
     var clubList = document.getElementById('club-content');
@@ -44,8 +48,8 @@ function getStudentInfo() {
     const endDiv = '</div>';
 
     personalInfo.innerHTML += 'Email: ' + studentInfo['email'] + newLine;
-    personalInfo.innerHTML += 'Grad Year: ' + startOfDivYear + editableDiv + studentInfo['gradYear'] + endDiv + newLine;
-    personalInfo.innerHTML += 'Major: ' + startOfDivMajor + editableDiv + studentInfo['major'] + endDiv + newLine;
+    personalInfo.innerHTML += 'Grad Year: ' + startOfDivYear + editableDiv + studentInfo['gradYear'] + pencilIcon + endDiv + newLine;
+    personalInfo.innerHTML += 'Major: ' + startOfDivMajor + editableDiv + studentInfo['major'] + pencilIcon + endDiv + newLine;
   });
 }
 
@@ -76,11 +80,13 @@ function createClubElement(text) {
 function saveProfileChanges() {
   const newYear = document.getElementById("edit-year").innerHTML;
   const newMajor = document.getElementById("edit-major").innerHTML;
+  const newName = document.getElementById("edit-name").innerHTML;
 
   var updateProfile = document.getElementById("update-profile");
 
   updateProfile.innerHTML = '<input type="hidden" name="new-year" value="' + newYear + '">';
   updateProfile.innerHTML += '<input type="hidden" name="new-major" value="' + newMajor + '">';
+  updateProfile.innerHTML += '<input type="hidden" name="new-name" value="' + newName + '">';
 
   document.forms['edit-profile'].submit();
 }
