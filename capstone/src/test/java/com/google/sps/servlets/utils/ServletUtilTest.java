ef9e512858446f4e19f1ff521e9f12474591904b
meghakak <meghakak1@gmail.com>
2020-08-12 00:03:28 +0000

    Remove unnecessary code for util tests.

--- capstone/src/test/java/com/google/sps/servlets/utils/ServletUtilTest.java
+++ capstone/src/test/java/com/google/sps/servlets/utils/ServletUtilTest.java
@@ -1,7 +1,5 @@
 package com.google.sps.servlets;
 
-import static org.mockito.Mockito.when;
-
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -15,7 +13,6 @@ import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,25 +28,14 @@ public final class ServletUtilTest {
   public static final String CLUB_1 = "Club 1";
 
   @Mock private HttpServletRequest request;
-  @Mock private HttpServletResponse response;
   private LocalServiceTestHelper localHelper =
       new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
   private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-  private Entity studentMegan;
-  private Entity club1;
 
   @Before
   public void setUp() throws Exception {
     MockitoAnnotations.initMocks(this);
     localHelper.setUp();
-
-    studentMegan = new Entity(MEGAN_EMAIL);
-    studentMegan.setProperty(Constants.PROPERTY_EMAIL, MEGAN_EMAIL);
-    studentMegan.setProperty(Constants.PROPERTY_CLUBS, ImmutableList.of());
-
-    club1 = new Entity("Club");
-    club1.setProperty(Constants.PROPERTY_NAME, CLUB_1);
-    club1.setProperty(Constants.MEMBER_PROP, ImmutableList.of(MEGAN_EMAIL));
   }
 
   @After
@@ -59,9 +45,10 @@ public final class ServletUtilTest {
 
   @Test
   public void getPropertyList_getEmptyClubList() throws ServletException, IOException {
+    Entity studentMegan = new Entity(MEGAN_EMAIL);
+    studentMegan.setProperty(Constants.PROPERTY_EMAIL, MEGAN_EMAIL);
+    studentMegan.setProperty(Constants.PROPERTY_CLUBS, ImmutableList.of());
     datastore.put(studentMegan);
-    localHelper.setEnvEmail(MEGAN_EMAIL).setEnvAuthDomain("google.com").setEnvIsLoggedIn(true);
-    when(request.getParameter(Constants.PROPERTY_EMAIL)).thenReturn(MEGAN_EMAIL);
 
     // Get student entity from Datastore
     Query query = new Query(MEGAN_EMAIL);
@@ -70,16 +57,17 @@ public final class ServletUtilTest {
     Assert.assertFalse(students.isEmpty());
     Assert.assertTrue(students.size() == 1);
     Entity student = students.get(0);
-    ImmutableList<String> clubList = ServletUtil.getPropertyList(student, Constants.PROPERTY_CLUBS);
 
-    Assert.assertEquals(ImmutableList.of(), clubList);
+    Assert.assertEquals(
+        ImmutableList.of(), ServletUtil.getPropertyList(student, Constants.PROPERTY_CLUBS));
   }
 
   @Test
   public void getPropertyList_getNonEmptyMemberList() throws ServletException, IOException {
+    Entity club1 = new Entity("Club");
+    club1.setProperty(Constants.PROPERTY_NAME, CLUB_1);
+    club1.setProperty(Constants.MEMBER_PROP, ImmutableList.of(MEGAN_EMAIL));
     datastore.put(club1);
-    localHelper.setEnvEmail(MEGAN_EMAIL).setEnvAuthDomain("google.com").setEnvIsLoggedIn(true);
-    when(request.getParameter(Constants.PROPERTY_NAME)).thenReturn(CLUB_1);
 
     // Get club entity from Datastore
     Query query =
@@ -90,9 +78,8 @@ public final class ServletUtilTest {
     Assert.assertFalse(clubs.isEmpty());
     Assert.assertTrue(clubs.size() == 1);
     Entity club = clubs.get(0);
-    ImmutableList<String> actualMemberList =
-        ServletUtil.getPropertyList(club, Constants.MEMBER_PROP);
 
-    Assert.assertEquals(ImmutableList.of(MEGAN_EMAIL), actualMemberList);
+    Assert.assertEquals(
+        ImmutableList.of(MEGAN_EMAIL), ServletUtil.getPropertyList(club, Constants.MEMBER_PROP));
   }
 }
