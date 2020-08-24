285ff833d04a79e4a85971cf9a21d8d4af23f6e3
meghakak <meghakak1@gmail.com>
2020-08-24 20:03:33 +0000

    Add styling for front page.

--- capstone/src/main/java/com/google/sps/servlets/utils/ServletUtil.java
+++ capstone/src/main/java/com/google/sps/servlets/utils/ServletUtil.java
@@ -47,8 +47,6 @@ public final class ServletUtil {
     // If you want to run this locally, you will need to replace this with your dev server URI
     // - then add "/oauth2callback" to the end of it and add that to you API console under
     // Authorized URIs.
-    // return
-    // "https://8080-4417b0ad-e7ff-4d2f-acc8-c9ddcf7b56d9.us-west1.cloudshell.dev/oauth2callback";
     // return "https://clubhub-step-2020.googleplex.com/oauth2callback";
     return "https://8080-4afd6625-e4a1-43f4-8d79-fc4c0cf1c87d.us-west1.cloudshell.dev/oauth2callback";
   }
